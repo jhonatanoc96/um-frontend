d9481d101acf93bcbd0b5ee855f645e1b8ec01ef
Jhonatan Ocampo <jhonatanocar96@gmail.com>
2024-02-22 12:38:45 -0500

    Implement basic pages to get product information

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,12 +1,40 @@
 import './Home.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+
+    if (!searchTerm) {
+      return alert('Por favor, ingrese un término de búsqueda.');
+    }
+    
+    // Redirect to /items?search=...
+    navigate(`/items?search=${searchTerm}`);
+
+  };
+
   return (
     <div className="Home">
       <header className="Home-header">
-        <p>
-          Home
-        </p>
+        <p>Búsqueda de productos</p>
+        <form onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
